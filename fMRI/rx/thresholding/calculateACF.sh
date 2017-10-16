6a1601785cb4cc5731e0115e80e43e9aceadaefb
katemills <klmills@uoregon.edu>
2017-10-16 14:43:39 -0700

    updated ACF script

--- fMRI/rx/thresholding/calculateACF.sh
+++ fMRI/rx/thresholding/calculateACF.sh
@@ -1,34 +1,15 @@
 #!/bin/bash
-#--------------------------------------------------------------
-#
-#SBATCH --job-name=acf_tresh
-#SBATCH --output=output/acf_thresh.log
-#SBATCH --error=output/ach_thresh_error.log
-#SBATCH --cpus-per-task=25
-#SBATCH --ntasks=1
-#SBATCH --mem-per-cpu=4000
-#SBATCH --partition=fat,short
 
 # This script calculates the smoothness (acf parameters) of RX residual files
-# using 3dFWHMx in AFNI and averages across all files by executing calculateACF.R
-# The average acf parameters will be saved as SPM_acf.txt in the code directory.
-
-# It also calculates the acf parameters for the AFNI 3dLME models and saves the 
-# parameters to text files in the code directory.
+# using 3dFWHMx in AFNI.
 
 module load prl afni
-
-# Specify variables
-# ------------------------------------------------------------------------------------------
-# thresholding code directory
-#codeDir=/projects/dsnlab/SFIC_Faces3/SFIC_faces3_scripts/fMRI/rx/thresholding
 
 # RX directories (where the residual files are)
 rxDir=/projects/dsnlab/SFIC_Faces3/fMRI/analysis/rx/AFNI/
 
 # AFNI 3dLME model names
-models=allAff_pds_female
-#models=(allAff_age allAff_pds_female allAff_pds_male allAff_test_female allAff_test_male eachAff_age eachAff_pds_female eachAff_pds_male eachAff_test_female eachAff_test_male)
+models=(allAff_age allAff_pds_female allAff_pds_male allAff_test_female allAff_test_male allAff_logtest_female allAff_logtest_male eachAff_age eachAff_pds_female eachAff_pds_male eachAff_test_female eachAff_test_male eachAff_logtest_female eachAff_logtest_male)
 
 # Estimate acf parameters for AFNI 3dLME models and save this output
 # ------------------------------------------------------------------------------------------
@@ -40,6 +21,3 @@ for model in "${models[@]}"; do
 	cd ../
 done
 
-# Execute calculateACF.R
-# ------------------------------------------------------------------------------------------
-#Rscript "${codeDir}"/calculateACF.R "${spmDir}"/residuals.txt > "${rxDir}"/thresholding/acf.txt
