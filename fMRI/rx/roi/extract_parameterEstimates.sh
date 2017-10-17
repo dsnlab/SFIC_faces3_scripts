da3db1197b4a5bcecc64100368f6f8c4f7bf64e9
katemills <klmills@uoregon.edu>
2017-10-17 13:24:57 -0700

    updated roi script

--- fMRI/rx/roi/extract_parameterEstimates.sh
+++ fMRI/rx/roi/extract_parameterEstimates.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-. ~/.bashrc
 
 # This script extracts mean parameter estimates and SDs within an ROI or parcel
 # from subject FX condition contrasts (condition > rest) for each wave. Output is 
@@ -14,7 +13,7 @@ output_dir='/projects/dsnlab/SFIC_Faces3/fMRI/analysis/rx/roi' #roi output direc
 
 # variables
 subjects=`cat /projects/dsnlab/SFIC_Faces3/SFIC_faces3_scripts/fMRI/rx/3dLME/subject_list.csv`
-waves=(t1 t2 t3) #waves or task names
+waves=(wave1 wave2 wave3) #waves or task names
 fx_cons=(con_0001 con_0002 con_0003 con_0004 con_0005) #fx con files to extract from
 rx_model=(eachAff_age)
 
@@ -26,7 +25,7 @@ for sub in ${subjects[@]}; do
 	for wave in ${waves[@]}; do 
 		for con in ${fx_cons[@]}; do 
 			for num in {1..20}; do 
-					echo ${sub} ${wave} ${con} ${num} `3dmaskave -sigma -quiet -mrange $num $num -mask $rxdir/$rx_model/Clust_mask+tlrc $con_dir/${sub}_${wave}_${con}.nii` >> $output_dir/paramEstimates_${rx_model}.txt
+					echo ${sub} ${wave} ${con} ${num} `3dmaskave -sigma -quiet -mrange $num $num -mask $rx_dir/$rx_model/Clust_mask+tlrc $con_dir/sub-L0${sub}_ses-${wave}_${con}.nii` >> $output_dir/paramEstimates_${rx_model}.txt
 			done
 		done
 	done
