7eea3df7ba7e5dbc8eef763d38a19ff131faa023
Kathryn LeeAnn Mills <klmills@uoregon.edu>
2017-10-17 18:55:55 -0700

    update afni2nii

--- fMRI/rx/3dLME/convert_AFNItoNIFTI.sh
+++ fMRI/rx/3dLME/convert_AFNItoNIFTI.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
-. ~/.bashrc
 
 # Define path to AFNI results directory, model names, and contrast numbers
 # ------------------------------------------------------------------------------------------
-resultsDir=/Volumes/psych-cog/dsnlab/MDC/functional-workshop/results/AFNI
-models=(3Ts all)
-selfOther=13
-age_selfOther=23
-age2_selfOther=25
+resultsDir=/projects/dsnlab/SFIC_Faces3/fMRI/analysis/rx/AFNI
+models=(eachAff_age eachAff_pds eachAff_logtest)
 
 # Convert AFNI contrasts to nifti files
 # ------------------------------------------------------------------------------------------
 # AFNI documentation https://afni.nimh.nih.gov/pub/dist/doc/program_help/3dAFNItoNIFTI.html
 for model in  "${models[@]}" ; do
-	3dAFNItoNIFTI -prefix "${resultsDir}"/self-other_"${model}" "${resultsDir}"/"${model}"+tlrc["${selfOther}"]
-	3dAFNItoNIFTI -prefix "${resultsDir}"/age.self-other_"${model}" "${resultsDir}"/"${model}"+tlrc["${age_selfOther}"]
-	3dAFNItoNIFTI -prefix "${resultsDir}"/age2.self-other_"${model}" "${resultsDir}"/"${model}"+tlrc["${age2_selfOther}"]
+	3dAFNItoNIFTI -prefix age eachAff_age+tlrc["1"]
+	3dAFNItoNIFTI -prefix "${resultsDir}"/age.self-other_"${model}" "${resultsDir}"/"${model}"+tlrc["1"]
+	3dAFNItoNIFTI -prefix "${resultsDir}"/age2.self-other_"${model}" "${resultsDir}"/"${model}"+tlrc["1"]
 done
